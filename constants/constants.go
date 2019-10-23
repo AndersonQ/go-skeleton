64fd2210bddb173965ac373e7758dad0a072de05
Eduardo Díaz <edjroz@users.noreply.github.com>
2019-10-23 18:19:21 -0400

    Organize constants alphabetically

--- constants/constants.go
+++ constants/constants.go
@@ -3,16 +3,15 @@ package constants
 // LogKey are constants to be used for log field names
 const (
 	// Keep it alphabetically sorted
-	// TODO: sort it alphabetically
 	LogKeyApp              = "_app"
 	LogKeyEnv              = "_env"
 	LogKeyHost             = "_host"
-	LogKeyTimestamp        = "_timestamp"
 	LogKeyHTTPMethod       = "_http_method"
+	LogKeyHTTPStatus       = "_http_status"
 	LogKeyRemoteAddr       = "_remote_addr"
-	LogKeyURLPath          = "_url_path"
-	LogKeyUserAgent        = "_user_agent"
 	LogKeyResquestDuration = "_request_duration_ms"
 	LogKeyResquestID       = "_request_id"
-	LogKeyHTTPStatus       = "_http_status"
+	LogKeyTimestamp        = "_timestamp"
+	LogKeyURLPath          = "_url_path"
+	LogKeyUserAgent        = "_user_agent"	
 )
